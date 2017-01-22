b02d6ee0c65070acffbdfcb300b57ba0ccef5220
THINKPAD-LGB\mrlgb <mrlgb@sina.com>
2017-01-22 14:27:32 +0800

    更新： 1\微调tab layout style

--- app/src/main/java/edu/hfuu/jccloud/tableUi/Main_Summary.java
+++ app/src/main/java/edu/hfuu/jccloud/tableUi/Main_Summary.java
@@ -6,38 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
 import edu.hfuu.jccloud.R;
 
 /**
- * Created by lgb on 21-01-2015.
+ * Created by lgb on 21-10-2016.
  */
 public class Main_Summary extends Fragment {
-    @Bind(R.id.btn_mainsummary_Save)
-    Button btnSave;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.main_summary,container,false);
-        ButterKnife.bind(this, v);
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "All data have been saved!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
         return v;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        ButterKnife.unbind(this);
-    }
 }
