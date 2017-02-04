16514285c7d729092fc20846fa9a0dbc90cde20b
THINKPAD-LGB\mrlgb <mrlgb@sina.com>
2017-02-04 21:31:12 +0800

    更新： 1、使用DATA PICKER 选择时间 2、验证逻辑添加

--- app/src/main/java/edu/hfuu/jccloud/tableUi/SZ01_Dynamic.java
+++ app/src/main/java/edu/hfuu/jccloud/tableUi/SZ01_Dynamic.java
@@ -1,5 +1,6 @@
 package edu.hfuu.jccloud.tableUI;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
@@ -11,9 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -40,12 +43,19 @@ public class SZ01_Dynamic extends Fragment {
     @Bind(R.id.inputLayout21)
     TextInputLayout input21;
 
+    @Bind(R.id.inputLayoutTime)
+    TextInputLayout inputLayoutTime;
+    @Bind(R.id.inputTimePicker)
+    EditText edtTime;
+
     @Bind(R.id.btn_SZ_O1_Dynamic_Add)
     Button btnAdd;
     @Bind(R.id.btn_SZ_O1_Dynamic_Delete)
     Button btnDel;
     @Bind(R.id.btn_SZ_O1_Dynamic_Save)
     Button btnSave;
+
+    int mHour, mMinute, mSecond;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -77,6 +87,32 @@ public class SZ01_Dynamic extends Fragment {
         mAdapter = new SampleSZ01Adapter(mDataSet, getContext());
         mRecyclerView.setAdapter(mAdapter);
 
+        edtTime.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        //To show current date in the datepicker
+                        final Calendar c = Calendar.getInstance();
+                        final int mHour = c.get(Calendar.HOUR_OF_DAY);
+                        final int mMinute = c.get(Calendar.MINUTE);
+
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                edtTime.setText(
+                                        new StringBuilder()
+                                                .append(pad(mHour)).append(":")
+                                                .append(pad(mMinute)));
+                            }
+                        }, 0, 0, true);
+                        timePickerDialog.show();
+                    }
+
+
+                });
+
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,7 +131,7 @@ public class SZ01_Dynamic extends Fragment {
                 int position = mDataSet.size() - 1;
                 mDataSet.remove(position);
                 mAdapter.notifyItemRemoved(position);
-                mRecyclerView.scrollToPosition(position-1);
+                mRecyclerView.scrollToPosition(position - 1);
                 Toast.makeText(getContext(), "Sample " + position + " Deleted!", Toast.LENGTH_SHORT).show();
             }
         });
@@ -103,7 +139,7 @@ public class SZ01_Dynamic extends Fragment {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( validateData() ) {
+                if (validateData()) {
                     Toast.makeText(getContext(), "All sample have been saved!", Toast.LENGTH_SHORT).show();
                 }
 
@@ -117,14 +153,23 @@ public class SZ01_Dynamic extends Fragment {
         boolean result = true;
 
         String name = edtSampleNo.getText().toString();
-        if (name == null || name.length()<3) {
+        if (name == null || name.length() < 3) {
             // We set the error message
             inputSampleNo.setError("请输入正确的样品编号（>3字符）");
             result = false;
-        }
-        else
-        // We remove error messages
-        inputSampleNo.setErrorEnabled(false);
+        } else
+            // We remove error messages
+            inputSampleNo.setErrorEnabled(false);
+
+        String time = edtTime.getText().toString();
+
+        if (time == null || time.length() < 3) {
+            // We set the error message
+            inputLayoutTime.setError("请输入正确的时间（）");
+            result = false;
+        } else
+            // We remove error messages
+            inputLayoutTime.setErrorEnabled(false);
 
         return result;
     }
@@ -136,12 +181,20 @@ public class SZ01_Dynamic extends Fragment {
 
     }
 
+    /** Add padding to numbers less than ten */
+    private static String pad ( int c){
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
+
 
     // load initial data
     public void loadData() {
         mDataSet.clear();
         for (int i = 0; i < 25; i++) {
-            SampleSZ01 sample = new SampleSZ01("Sample " + i,  "index " + i);
+            SampleSZ01 sample = new SampleSZ01("Sample " + i, "index " + i);
             mDataSet.add(sample);
         }
     }
--- app/src/main/java/edu/hfuu/jccloud/tableUi/SZ01_Static.java
+++ app/src/main/java/edu/hfuu/jccloud/tableUi/SZ01_Static.java
@@ -1,38 +1,106 @@
 package edu.hfuu.jccloud.tableUI;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import edu.hfuu.jccloud.R;
 
+import static edu.hfuu.jccloud.R.id.inputDatePicker;
+
 /**
  * Created by lgb on 21-01-2015.
  */
+
 public class SZ01_Static extends Fragment {
     @Bind(R.id.btn_SZ_O1_Static_Save)
     Button btnSave;
 
+    @Bind(R.id.inputLayoutDate)
+    TextInputLayout inputLaDate;
+
+    @Bind(inputDatePicker)
+    EditText inputDate;
+
+    int mYear, mMonth, mDay;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.sz01_static,container,false);
+        View v = inflater.inflate(R.layout.sz01_static, container, false);
         ButterKnife.bind(this, v);
+
+        inputDate.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        //To show current date in the datepicker
+                        final Calendar mcurrentDate = Calendar.getInstance();
+                        mYear = mcurrentDate.get(Calendar.YEAR);
+                        mMonth = mcurrentDate.get(Calendar.MONTH);
+                        mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                        DatePickerDialog mDatePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                            public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                                // TODO Auto-generated method stub
+                    /*      Your code   to get date and time    */
+                                mcurrentDate.set(Calendar.YEAR, mYear);
+                                mcurrentDate.set(Calendar.MONTH, mMonth);
+                                mcurrentDate.set(Calendar.DAY_OF_MONTH, mDay);
+                                Calendar newDate = Calendar.getInstance();
+                                newDate.set(selectedyear, selectedmonth, selectedday);
+                                String myFormat = "dd/MMM/yyyy"; //In which you need put here
+                                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.CHINA);
+                                inputDate.setText(sdf.format(newDate.getTime()));
+                            }
+                        }, mYear, mMonth, mDay);
+                        mDatePicker.setTitle("Select date");
+                        mDatePicker.show();
+                    }
+                });
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "All data have been saved!", Toast.LENGTH_SHORT).show();
+                if (validateData()) {
+                    Toast.makeText(getContext(), "All sample have been saved!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         return v;
+    }
+
+
+    private boolean validateData() {
+        boolean result = true;
+        String name = inputDate.getText().toString();
+
+        if (name == null || name.length() < 3) {
+            // We set the error message
+            inputLaDate.setError("请输入正确的日期（）");
+            result = false;
+        } else
+            // We remove error messages
+            inputLaDate.setErrorEnabled(false);
+
+        return result;
     }
 
     @Override
